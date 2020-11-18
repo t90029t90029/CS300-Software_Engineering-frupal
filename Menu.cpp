a1d888756ea2a813950e79234ef7d01f2a856cb3
Jared Collums <jcollums@babbage.cs.pdx.edu>
2020-11-17 19:10:54 -0800

    Menu separator displaying

---
+++ Menu.cpp
@@ -0,0 +1,11 @@
+#include "Menu.h"
+
+void Menu::display(Player *player, Map *map) {
+/*
+  int x, y;
+  player->locate(&y, &x);
+  int energy = player->getEnergy();
+  int money = player->getMoney();
+*/
+  mvvline(0, COLS - MENU_WIDTH, MENU_BORDER, LINES);
+}
