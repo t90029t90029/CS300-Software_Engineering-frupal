b6b4d6fb85e85f41ff3555b362e24b1482054d3d
Shang Chun Lin <shanglin@babbage.cs.pdx.edu>
2020-11-21 04:33:46 -0800

    item feature added

---
+++ Clue.cpp
@@ -0,0 +1,63 @@
+#include "Item.h"
+
+// Randomly generate Clue
+Clue::Clue(){
+  type = ClueType(rand() % 2);
+  toggle = 0;
+  switch(type) {
+    case TRUTH:
+	    isTrue = "TRUTH";
+	    break;
+    case LIE:
+	    isTrue = "LIE";
+	    break;
+    default:
+	    break;
+  } 
+}
+  
+Clue::~Clue(){
+  content.clear();
+}
+  
+int Clue::getCost(){
+  return 0;
+}
+
+int Clue::getStrength(){
+  return 0;
+}
+
+int Clue::getObstacle(){
+  return 0;
+}
+
+// Get hint
+int Clue::getDetails(string & source){
+  if(content.length() > 0){
+    source = content;
+    return 1;
+  }
+  return 0;
+}
+
+int Clue::getMoney(){
+  return 0;
+}
+
+// Get true/lie
+int Clue::getTruth(){
+  if(type == TRUTH)
+    return 1;
+  return 0;
+}
+
+// Set hint
+void Clue::setClue(string clue){
+  content = clue;
+  toggle = 1;
+}
+ 
+int Clue::getToggle(){
+  return toggle;
+}
